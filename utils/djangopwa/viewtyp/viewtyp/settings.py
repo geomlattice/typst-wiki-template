1d753ce80adb1b7dea113e21e610f1faab21c220
Keanu Clark <markov.particle@gmail.com>
2026-03-21 22:41:15 -0400

    django pwa basic testing almost complete

--- utils/djangopwa/viewtyp/viewtyp/settings.py
+++ utils/djangopwa/viewtyp/viewtyp/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = "viewtyp.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["viewtyp/templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
